d29fa14340154902261bb471427aa6099904197b
unknown <christianmabeladi@gmail.com>
2023-10-15 02:01:11 +0200

    select elements html in js and creation event

--- calculator.js
+++
@@ -1,8 +0,0 @@
-// Do not remove this function definition and its export
-// this function should return the result of the current operation
-function calculate(expression) {
-  // ecrivvez votre code ci-dessus
-  return expression;
-}
-
-
