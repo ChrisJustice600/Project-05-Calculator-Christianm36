9a8d81fe2e71b47ec9256587278875dabb823cbf
unknown <christianmabeladi@gmail.com>
2023-10-15 02:33:34 +0200

    add function andlebuttonclick

--- script.js
+++ script.js
@@ -1,209 +1,33 @@
+const form = document.querySelector("form");
+const buttons = form.querySelectorAll("button");
+const label = document.querySelector("#input");
+const userInput = form.elements["userInput"];
 
 
-// TODO: Faire la manipulation du DOM dans ce fichier
-// console.log("hello")
-const buttons = document.querySelectorAll(".numpad");
-const inputText = document.querySelector("input");
-const plus = document.getElementById("plus");
-const equal = document.getElementById("equals");
-const moins = document.getElementById("minus");
-const times = document.getElementById("times");
-const divideby = document.getElementById("divideby");
-const percentage = document.getElementById("percentage");
-const plusoumoins = document.getElementById("plusoumoins");
-// console.log(minus)
-const calcul = document.getElementById("calcul");
-const reset = document.getElementById("reset")
-const clear = document.getElementById("clear")
+// INITIALISATION 
+label.textContent = "";
+userInput.value = "";
 
 
-clear.addEventListener("click", (e)=>{
-    e.preventDefault();
-
- 
-    const valeurActuelle = inputText.value;
-    const nouvelleValeur = valeurActuelle.slice(0, -1); // Retirer le dernier caractère
-
-    inputText.value = nouvelleValeur;
-
-})
-reset.addEventListener("click", (e)=>{
-    e.preventDefault();
-
-    calcul.textContent = "";
-
-    inputText.value = "";
-
-})
-
-inputText.addEventListener("keydown", (event) => {
-  event.preventDefault();
-});
-
-// console.log(inputText.value)
-// console.log(buttons)
-inputText.value = "";
-calcul.textContent = " ";
-
-buttons.forEach((button) => {
-    // console.log(button)
-    button.addEventListener("click", (e) => {
-        const valeur = inputText.value;
-  
-    if (valeur.length > 10) {
-      // Tronquer ou supprimer les caractères supplémentaires
-      inputText.value = valeur.slice(0, 10);
-      
-      console.log("La saisie est limitée à 10 caractères !");
-      
-      // Vous pouvez effectuer d'autres actions ici si nécessaire
-    }
-        
-    /////////-------------- Verifier le virgul decimal dans ma chaine---------------------///////
-    if (e.target.textContent === "." && inputText.value.includes(".")) {
-      return;
-    }
-    inputText.value += e.target.textContent;
-      console.log(inputText.value);
-    //   console.log(inputText.value.length)
-
-
-    //////////------------------ Gestion de zero au debut de l'Input------------//////////////
-    if(inputText.value[0] === "0" && inputText.value[1] === "0"){
-        console.log("ok ok ")
-        inputText.value = inputText.value.slice(0, -1);
-    // Sortir de la fonction pour empêcher toute autre action après avoir réinitialisé la valeur
-    return;
-    }
-    })
-
-})
-
-let forCal = "";
-plus.addEventListener("click", (e)=>{
-    e.preventDefault();
-    // inputText.value += e.target.textContent
-    if (calcul.textContent.includes("+")) {
-        return;
+// CREATION DE LA FONCTION D'EVNEMENT D'ECOUTE 
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+      switch (this.type) {
+        case "submit":
+          handleSubmitClick(this.textContent);
+          break;
+        case "reset":
+          handleResetClick(label, form);
+          break;
+        case "button":
+          handleButtonClick(this.textContent, userInput);
+          break;
+        default:
+          break;
       }
-    if(forCal.includes("=")) {
-        calcul.textContent = "";
-        calcul.textContent = inputText.value + " "+e.target.textContent+" ";
-        inputText.value = "";
-    }else {
-        calcul.textContent += inputText.value + " "+e.target.textContent+" ";
+    });
+  });
 
-    inputText.value = "";
-
-    console.log(calcul.textContent);
-    }
-    
-    
-})
-moins.addEventListener("click", (e)=>{
-    e.preventDefault();
-    // inputText.value += e.target.textContent
-    if (calcul.textContent.includes("-")) {
-        return;
-      }
-
-    calcul.textContent += inputText.value + " "+e.target.textContent+" ";
-   
-    inputText.value = "";
-    // calcul.textContent = e.target.textContent
-    // console.log(calcul.textContent);
-})
-
-times.addEventListener("click", (e)=>{
-    e.preventDefault();
-    if (calcul.textContent.includes("*")) {
-        return;
-      }
-    if(forCal.includes("=")) {
-        calcul.textContent = "";
-        let foisContent = e.target.textContent.replace(/×/g, "*");
-        calcul.textContent = inputText.value + " "+foisContent+" ";
-        inputText.value = "";
-    }else {
-        let foisContent = e.target.textContent.replace(/×/g, "*");
-        calcul.textContent += inputText.value + " "+foisContent+" ";
-        inputText.value = "";
-
-    console.log(calcul.textContent);
-    }
-
-})
-divideby.addEventListener("click", (e)=>{
-    e.preventDefault();
-    if (calcul.textContent.includes("/")) {
-        return;
-      }
-
-if(forCal.includes("=")) {
-    calcul.textContent = "";
-    let foisContent = e.target.textContent.replace(/÷/g, "/");
-    calcul.textContent = inputText.value + " "+foisContent+" ";
-    inputText.value = "";
-}else {
-    let foisContent = e.target.textContent.replace(/÷/g, "/");
-    calcul.textContent += inputText.value + " "+foisContent+" ";
-    inputText.value = "";
-
-console.log(calcul.textContent);
+function handleButtonClick(textContent, userInput){
+    userInput.value = `${textContent}${userInput.value}`
 }
-// calcul.textContent = e.target.textContent
-// console.log(calcul.textContent)
-
-})
-
-
-equal.addEventListener("click", (e)=> {
-    e.preventDefault();
-    let res = "";
-    
-    // console.log(inputText.value)
-    // console.log(calcul.textContent + inputText.value)
-    calcul.textContent += inputText.value;
-
-    console.log(calcul.textContent);
-    inputText.value = eval(calcul.textContent)
-    res =  inputText.value;
-    // console.log(res)
-    calcul.textContent +=" "+ e.target.textContent
-    forCal = calcul.textContent; 
-    console.log(calcul.textContent);
-
-    console.log(inputText.value);
-})
-percentage.addEventListener("click", (e)=>{
-    e.preventDefault();
-  
-let foisContent = e.target.textContent.replace(/÷/g, "/");
-console.log(inputText.value);
-
-calcul.textContent += inputText.value;
-    console.log(calcul.textContent);
-    inputText.value = eval(calcul.textContent)
-    inputText.value =  inputText.value/100
-
-})
-
-plusoumoins.addEventListener("click", (e)=>{
-    e.preventDefault();
-
-    console.log(inputText.value);
-    // if (e.target.textContent === "." && inputText.value.includes(".")) {
-    //     return;
-    //   }
-
-    if (inputText.value.indexOf("+") === -1) {
-        // if (inputText.value.includes("-")) {
-        //     return;
-        //   }
-        inputText.value = "-" + inputText.value
-    }else{
-        inputText.value = "" + inputText.value
-    }  
-
-   
-})
